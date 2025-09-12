159c26a023a65fb438b9a929e98e3f15369cd92b
AndrewMatzureff <andrewmatzureff@gmail.com>
2025-09-11 18:35:13 -0700

    Implement MazeLoader::ParseActors

--- Assets/scripts/MazeLoader.cs
+++ Assets/scripts/MazeLoader.cs
@@ -1,4 +1,5 @@
 ﻿using MazeNodes;
+using MazeTiles;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -41,22 +42,160 @@ public class MazeLoader : MonoBehaviour {
 ║••••••••••••••••••••••••••║
 ╚══════════════════════════╝";
 
+    public GameObject Maze;
+
+    // Empty
+    public GameObject p_NullSpace;
+    public GameObject p_NullShiftLeft;
+    public GameObject p_NullShiftRight;
+
+    // Actors
+    public GameObject p_PacMan;
+    public GameObject p_Ghost;
+    public GameObject p_PowerPellet;
+    public GameObject p_Pellet;
+
+    // Single Pipes
+    public GameObject p_SinglePipeHorizontal;
+    public GameObject p_SinglePipeVertical;
+
+    // Single Elbows
+    public GameObject p_SingleElbowTopLeft;
+    public GameObject p_SingleElbowTopRight;
+    public GameObject p_SingleElbowBottomLeft;
+    public GameObject p_SingleElbowBottomRight;
+
+    // Double Pipes
+    public GameObject p_DoublePipeHorizontal;
+    public GameObject p_DoublePipeVertical;
+
+    // Double Elbows
+    public GameObject p_DoubleElbowTopLeft;
+    public GameObject p_DoubleElbowTopRight;
+    public GameObject p_DoubleElbowBottomLeft;
+    public GameObject p_DoubleElbowBottomRight;
+
+    // Ghost Home
+    public GameObject p_GhostHomeTopLeft;
+    public GameObject p_GhostHomeTopRight;
+    public GameObject p_GhostHomeBottomLeft;
+    public GameObject p_GhostHomeBottomRight;
+    public GameObject p_GhostDoorLeft;
+    public GameObject p_GhostDoorway;
+    public GameObject p_GhostDoorRight;
+
+    private Dictionary<Tile, GameObject> prefabsByTile;
+
     // Use this for initialization
     void Start () {
-        new ConduitNode(MazeTiles.Tile.DOUBLE_ELBOW_BOTTOM_LEFT, 0, 0);
-        List<string> lines = new List<string>(MazeString.Split('\n'));
+        prefabsByTile = new Dictionary<Tile, GameObject> {
+            { Tile.NULL_SPACE,                  p_NullSpace },
+            { Tile.NULL_SHIFT_LEFT,             p_NullShiftLeft },
+            { Tile.NULL_SHIFT_RIGHT,            p_NullShiftRight },
+
+            { Tile.PAC_MAN,                     p_PacMan },
+            { Tile.GHOST,                       p_Ghost },
+            { Tile.POWER_PELLET,                p_PowerPellet },
+            { Tile.PELLET,                      p_Pellet },
+
+            { Tile.SINGLE_PIPE_HORIZONTAL,      p_SinglePipeHorizontal },
+            { Tile.SINGLE_PIPE_VERTICAL,        p_SinglePipeVertical },
+
+            { Tile.SINGLE_ELBOW_TOP_LEFT,       p_SingleElbowTopLeft },
+            { Tile.SINGLE_ELBOW_TOP_RIGHT,      p_SingleElbowTopRight },
+            { Tile.SINGLE_ELBOW_BOTTOM_LEFT,    p_SingleElbowBottomLeft },
+            { Tile.SINGLE_ELBOW_BOTTOM_RIGHT,   p_SingleElbowBottomRight },
+
+            { Tile.DOUBLE_PIPE_HORIZONTAL,      p_DoublePipeHorizontal },
+            { Tile.DOUBLE_PIPE_VERTICAL,        p_DoublePipeVertical },
+
+            { Tile.DOUBLE_ELBOW_TOP_LEFT,       p_DoubleElbowTopLeft },
+            { Tile.DOUBLE_ELBOW_TOP_RIGHT,      p_DoubleElbowTopRight },
+            { Tile.DOUBLE_ELBOW_BOTTOM_LEFT,    p_DoubleElbowBottomLeft },
+            { Tile.DOUBLE_ELBOW_BOTTOM_RIGHT,   p_DoubleElbowBottomRight },
+
+            { Tile.GHOST_HOME_TOP_LEFT,         p_GhostHomeTopLeft },
+            { Tile.GHOST_HOME_TOP_RIGHT,        p_GhostHomeTopRight },
+            { Tile.GHOST_HOME_BOTTOM_LEFT,      p_GhostHomeBottomLeft },
+            { Tile.GHOST_HOME_BOTTOM_RIGHT,     p_GhostHomeBottomRight },
+            { Tile.GHOST_DOOR_LEFT,             p_GhostDoorLeft },
+            { Tile.GHOST_DOORWAY,               p_GhostDoorway },
+            { Tile.GHOST_DOOR_RIGHT,            p_GhostDoorRight },
+        };
+        
+        List<string> lines = new List<string>(MazeString.Trim().Split(new[] { "\r\n", "\n" }, StringSplitOptions.RemoveEmptyEntries));
         ParseActors(gameObject, lines);
-        ParseConduits(lines);
+        //ParseConduits(lines);
     }
 
     // Update is called once per frame
     void Update() {}
 
-    void ParseActors(GameObject maze, List<String> mazeGrid) {}
+    private void ParseActors(GameObject maze, List<String> mazeGrid) {
+        int rows = mazeGrid.Count;
+        Dictionary<GameObject, Tile> tiles = new Dictionary<GameObject, Tile>();
+        GameObject gameObject = null;
 
-    List<ConduitNode> BuildConduitGraph(GameObject gameObject, List<string> mazeGrid) { return null; }
+        for (int r = 0; r < rows; r++) {
+            Debug.Log("\nr: " + r + ", Count(c => c == 13): " + mazeGrid[r].Count(f => f == 13));
+            int columns = mazeGrid[r].Length;
 
-    void ParseConduits(List<String> mazeGrid) {}
+            for (int c = 0; c < columns; c++) {
+                char actor = mazeGrid[r][c];
+                Debug.Log("c: " + c + ", actor: " + actor + " (" + (int) actor + ")");
+                if (actor == '\n') Debug.Log("NEW LINE!!!");
 
-    void ParseConduits(ConduitNode root, Action<ConduitNode> consumer) {}
+                if (gameObject == null) {
+                    gameObject = new GameObject();
+                    gameObject.transform.SetParent(maze.transform);
+                    tiles.Add(gameObject, Util.Tile(actor));
+                } else tiles[gameObject] = Util.Tile(actor);
+
+                float subcellOffset
+                  = ( c > 0
+                    ? mazeGrid[r][c - 1] == '<' ? -0.5f : 0
+                    : 0 )
+                  + ( c < columns - 1
+                    ? mazeGrid[r][c + 1] == '>' ? +0.5f : 0
+                    : 0 );
+
+                if (Util.IsActor(tiles[gameObject])) {
+                    // spawn actor
+                    Debug.Log("IsActor");
+                    //float offsetX = columns 
+                    // TODO: find a way to centralize the pixels per unit value between the spritesheet and the position helper in a constant.
+                    Instantiate(prefabsByTile[tiles[gameObject]], maze.transform.position + PositionAt(c, r, columns, rows, 16, 100f, subcellOffset), Quaternion.identity);
+                    gameObject = null;
+                }
+            }
+        }
+
+        if (gameObject != null && !Util.IsActor(tiles[gameObject])) {
+            tiles.Remove(gameObject);
+            Destroy(gameObject);
+        }
+
+        //tiles.Select(kv => "");
+
+        tiles.Clear();
+        // Object o = ConduitNode.class;
+        // ConduitNode.build(false, 0,0,new Dictionary<int, ConduitNode>(),mazeGrid);
+    }
+
+    private Vector3 PositionAt(int c, int r, int columns, int rows, int tilePixelDimensions, float pixelsPerUnit, float subcellOffset) {
+        return new Vector3(
+            ((c + subcellOffset)    * tilePixelDimensions) / pixelsPerUnit - columns    * tilePixelDimensions / (pixelsPerUnit * 2) + ((columns & 1) == 1 ? 0 : 0.5f * tilePixelDimensions / pixelsPerUnit),
+            ((rows - r - 1)         * tilePixelDimensions) / pixelsPerUnit - rows       * tilePixelDimensions / (pixelsPerUnit * 2) + ((rows    & 1) == 1 ? 0 : 0.5f * tilePixelDimensions / pixelsPerUnit)
+        );
+    }
+
+    List<ConduitNode> BuildConduitGraph(GameObject gameObject, List<string> mazeGrid) {
+        return null;
+    }
+
+    void ParseConduits(List<String> mazeGrid) {
+    }
+
+    void ParseConduits(ConduitNode root, Action<ConduitNode> consumer) {
+    }
 }
